0a97ce0cb101eef72c0cee185f120da296773b91
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-27 00:02:22 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+import logger from './middleware/logger.js';
+import errorHandler from './middleware/error.js';
+import posts from './routes/posts.js';
+import { fileURLToPath } from 'url'
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//Body parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Logger Middleware
+app.use(logger);
+
+//setup static folder for quick easy routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes;
+app.use('/api/posts', posts);
+
+// error handler
+app.use(errorHandler);
+
+
+
+
+
+app.listen(PORT, () => {console.log(`Odogwu running on ${PORT}`)});
