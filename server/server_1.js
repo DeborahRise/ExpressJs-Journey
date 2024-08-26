bfdc7a309af98768a6e34e6345d6e85ce0bba329
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-26 23:59:57 +0100

    server_1.js

---
+++ server/server_1.js
@@ -0,0 +1,15 @@
+//Basic method for reading from a file in an express server
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+app.listen(8000, () => {console.log('Baby running on 8000')});
