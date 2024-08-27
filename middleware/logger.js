f07c150ee426e144d27b494e85fa86b3f806c90a
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-27 02:39:27 +0100

    Modified middleware/logger.js

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,22 @@
+import colors from 'colors';
+
+
 // Logger Middleware
 const logger = (req, res, next) => {
-    console.log(`${req.method}: //${req.protocol}${req.get('host')} ${req.originalUrl}`);
+    // define an object for the colors and corresponding method
+    const methodColors = {
+        GET: 'green',
+        POST: 'blue',
+        PUT: 'yellow',
+        DELETE: 'red'
+    }
+
+    // initialize the colour variable
+    const color = methodColors[req.method] || 'white'
+
+    console.log(
+        `${req.method}: //${req.protocol}${req.get('host')} ${req.originalUrl}`
+    [color]);
     next();
 };
 
