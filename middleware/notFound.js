c5617463fac70b5f97690ca5aef21eb82e113fe8
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-27 02:39:27 +0100

    Modified middleware/notFound.js

--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,1 +1,10 @@
 // NotFound Middleware
+
+
+const notFound = (req, res, next) => {
+    const error = new Error('Page Not Found');
+    error.status = 404;
+    next(error);
+};
+
+export default notFound;
