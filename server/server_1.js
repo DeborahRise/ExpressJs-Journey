5a98b9cc0dceb29433376a43b0d6ba23eb2404dd
DeborahRise <debie.rise.bookings@gmail.com>
2024-09-02 09:27:41 +0100

    Update: Enhanced server_1.js with improved error handling and logging features

--- server/server_1.js
+++ server/server_1.js
@@ -13,3 +13,5 @@ app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 app.listen(8000, () => {console.log('Baby running on 8000')});
+
+// note: the url on the browser must include the .html extension to work
